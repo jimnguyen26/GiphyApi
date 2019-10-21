21a8ed9e1596950a037d1bb55c2e7272d682cbd2
Jim <jimmybnguyen@yahoo.com>
2019-10-21 14:52:41 -0700

    api key added

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ $(document).ready(function() {
 console.log('hello world');
 });
 
+apiKey = 'jrpNV9H93rFtvYVzLvedJKvh2j48ZUj8';
+
+url = 'https://api.giphy.com/v1/gifs/search?api_key=jrpNV9H93rFtvYVzLvedJKvh2j48ZUj8&q=&limit=25&offset=0&rating=G&lang=en';
