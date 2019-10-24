91c7ff4b139baf85dc7bdfcf262a88a92ee8c318
Jim <jimmybnguyen@yahoo.com>
2019-10-24 13:48:18 -0700

    got gif to append to DOM

--- app.js
+++ app.js
@@ -1,51 +1,68 @@
 $(document).ready(function() {
-console.log('hello world');
-});
-
-// apiKey = 'jrpNV9H93rFtvYVzLvedJKvh2j48ZUj8';
-
-//url = 'https://api.giphy.com/v1/gifs/search?api_key=jrpNV9H93rFtvYVzLvedJKvh2j48ZUj8&q=&limit=25&offset=0&rating=G&lang=en';
-
-var shows = ["Doug", "Salute Your Shorts", "Step by Step"];
-
-function displayTvShow() {
-
-    var shows = $(this).attr("data-name");
-    var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${shows}&limit=10&offset=0&rating=G&lang=en`;
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-    var showDiv = $("<div class='show'>");
-    var imgURL = response.Poster;
-    var image = $("<img>").attr("src", imgURL);
-    showDiv.append(image);
-    $("#tvShowView").prepend(showDiv);
+    console.log('hello world');
     });
-}
+    
+    apiKey = 'jrpNV9H93rFtvYVzLvedJKvh2j48ZUj8';
+    
+    var shows = ["Doug", "Salute Your Shorts", "Step by Step"];
+    
+    function displayTvShow() {
+    
+        var show = $(this).attr("data-name");
+        var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${show}&limit=10&offset=0&rating=G&lang=en`;
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            // console.log(response);
+            
+            var results = response.data;
+            console.log(results);
 
 
-
-function renderButtons() {
-    $("#buttonRow").empty();
-    for (var i = 0; i < shows.length; i++) {
-    var a = $("<button>");
-    a.addClass("showBtn");
-    a.attr("data-name", shows[i]);
-    a.text(shows[i]);
-    $("#buttonRow").append(a);
+            var showDiv = $("<div class='show'>");
+            
+            // var urlLink = results;
+            
+            var showImage = $("<img>");
+            showImage.attr("src", response.data[0].images.fixed_height.url);
+            showDiv.append(showImage);
+            
+            // var imgURL = $("<img>");
+            // imgURL.attr("src", response.data[0].url);
+            // showDiv.append(imgURL);
+            
+            var rating = response.data[0].rating;
+            console.log(rating);
+            var ratingLink = $('<p>').text("Rating: " + rating);
+            showDiv.append(ratingLink);
+            
+            $("#tvShowView").prepend(showDiv);
+        });
     }
-}
-
-
-$("#addTvShow").on("click", function(event) {
-    event.preventDefault();
-    var tvShow = $("tvShowInput").val().trim();
-    shows.push(tvShow);
+    
+    
+    
+    function renderButtons() {
+        $("#buttonRow").empty();
+        for (var i = 0; i < shows.length; i++) {
+        var a = $("<button>");
+        a.addClass("showBtn");
+        a.attr("data-name", shows[i]);
+        a.text(shows[i]);
+        $("#buttonRow").append(a);
+        }
+    }
+    
+    
+    $("#addTvShow").on("click", function(event) {
+        event.preventDefault();
+        var tvShow = $("tvShowInput").val().trim();
+        shows.push(tvShow);
+        renderButtons();
+    });
+    
+    $(document).on("click", ".showBtn", displayTvShow);
+    
     renderButtons();
-});
-
-$(document).on("click", ".showBtn", displayTvShow);
-
-renderButtons();
